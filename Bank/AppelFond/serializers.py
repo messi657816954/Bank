58130acffe4ebe3dab214353a862e22e671691ab
valery <aristide1.messi1@gmail.com>
2019-08-08 11:23:15 +0100

    Modifications views AppelFond

--- Bank/AppelFond/serializers.py
+++ Bank/AppelFond/serializers.py
@@ -1,58 +1,28 @@
 from rest_auth.registration.serializers import RegisterSerializer
 from rest_framework import serializers, fields
-from AppelFond.models import User, Tbanque, TappelFonds, TPlafond
+#from AppelFond.models import User, Tbanque, TappelFonds, TPlafond
+
 
 ############################# philippe ####################################
+from AppelFond.models import Tbanque, TappelFonds, TPlafond
+
 
 class banqSerializers(serializers.ModelSerializer):
-
     class Meta:
         model = Tbanque
         fields = ('nom_banq', 'code_banq', 'code_agence', 'url_core',)
 
 
-
-
 class appelsSerializers(serializers.ModelSerializer):
-
     class Meta:
         model = TappelFonds
-        fields = ('montant', 'date', 'compte_id', 'compte_numero', 'numero_recharge', 'client_id', 'client_nom', 'client_prenom', 'status','banque_id',)
-
+        fields = (
+        'montant', 'date', 'compte_id', 'compte_numero', 'numero_recharge', 'client_id', 'client_nom', 'client_prenom',
+        'status', 'banque_id',)
 
 
 class plafondSerializers(serializers.ModelSerializer):
-
     class Meta:
         model = TPlafond
-        fields = ('montant_max', 'montant_min', 'montant_cumul_max_week', 'montant_cumul_max_month', 'seuil_validation', 'client_id', 'banque_id', 'libelle')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        fields = ('montant_max', 'montant_min', 'montant_cumul_max_week', 'montant_cumul_max_month', 'seuil_validation',
+                  'client_id', 'banque_id', 'libelle')
